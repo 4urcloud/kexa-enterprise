150d3f22382a9a3dd943e10ac53c35cc22a5c497
Patrick szymkowiak <patrick.szymkowiak@innovtech.eu>
2025-11-14 13:33:02 +0100

    crt script

--- docker-compose-public/createbdds.sh
+++ docker-compose-public/createbdds.sh
@@ -4,10 +4,10 @@ set -u
 
 create_user_and_database() {
 	local database=$1
-	echo "  Création de la base de données '$database'..."
+	echo "  create '$database'..."
 	
 	if psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --list | grep -q "$database"; then
-		echo "  La base '$database' existe déjà, on passe."
+		echo "  bdd'$database' exist"
 	else
 		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 		    CREATE DATABASE $database;
