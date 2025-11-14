9f68a9506c8342392cb277c8088a4f5cffa68f0d
Patrick szymkowiak <patrick.szymkowiak@innovtech.eu>
2025-11-14 13:44:58 +0100

    crt script password bdd

--- docker-compose-public/createbdds.sh
+++ docker-compose-public/createbdds.sh
@@ -2,18 +2,21 @@
 set -e
 set -u
 
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
 create_user_and_database() {
-	local database=$1
-	echo "  create '$database'..."
-	
-	if psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --list | grep -q "$database"; then
-		echo "  bdd'$database' exist"
-	else
-		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		    CREATE DATABASE $database;
-		    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+    local database=$1
+    echo "  Checking '$database'..."
+    
+    if psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname='$database'" | grep -q 1; then
+        echo "  Database '$database' already exists"
+    else
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+            CREATE DATABASE $database;
+            GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
-	fi
+        echo "  Database '$database' created"
+    fi
 }
 
 create_user_and_database "keycloak"
